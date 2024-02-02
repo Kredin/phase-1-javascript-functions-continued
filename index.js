fcaf762a6a8b799c2253390bcc9738b411fd3222
Blade <bladeprojectz@gmail.com>
2024-02-02 17:02:25 +0800

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+function saturdayFun(value = "roller-skate"){
+    return `This Saturday, I want to ${value}!`
+}
+
+function mondayWork(value = 'go to the office'){
+    return `This Monday, I will ${value}.`
+}
+
+function wrapAdjective(value1){
+    return function inner(value2 = "special"){
+        return `You are ${value1}${value2}${value1}!`
+    }
+
+}
+
+
